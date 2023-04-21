517ca84f98147c6a0ab6933e342d87a74db58821
Vishal Lahane <vishal.lahane@corpnet.co.in>
2023-04-21 17:29:44 +0530

    composite grap initialized

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,13 +1,19 @@
 import React from "react";
+import ReactFC from "react-fusioncharts";
+import FusionCharts from "fusioncharts";
+import MultiLevelPie from "fusioncharts/fusioncharts.charts";
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
 import AdminLayout from "../layout/admin";
+import CompositeChart from "../components/charts/CompositeChart";
 
+ReactFC.fcRoot(FusionCharts, MultiLevelPie, FusionTheme);
 const DashboardPage = () => {
   return (
     <AdminLayout pageTitle="Dashboard">
       <section className="section dashboard">
         <div className="card">
           <div className="card-body pt-3">
-            <div className="row">charts will go here</div>
+            <CompositeChart />
           </div>
         </div>
       </section>
